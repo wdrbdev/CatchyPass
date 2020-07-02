f2cc8c70d8342c2986cf4c97e88a1ca139aed932
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-02 21:32:18 +0800

    feat(database): Initialize script for database seeding

---
+++ mongo/mongo-init.js
@@ -0,0 +1,49 @@
+db.sentence.updateOne(
+  { description: "1st seed data", isSeed: true },
+  {
+    $set: {
+      keywords: ["seed"],
+      startTime: new Date("2020-07-01T08:00:00Z"),
+      endTime: null,
+      resultSentence: null,
+      isCompleted: false,
+    },
+  },
+  {
+    upsert: true,
+  }
+);
+
+db.sentence.updateOne(
+  { description: "2nd seed data", isSeed: true },
+  {
+    $set: {
+      keywords: ["init"],
+      startTime: new Date("2020-07-02T23:00:00Z"),
+      endTime: null,
+      resultSentence: null,
+      isCompleted: true,
+      status: "Invalid vocabulary. Please try again.",
+    },
+  },
+  {
+    upsert: true,
+  }
+);
+db.sentence.updateOne(
+  { description: "3rd seed data", isSeed: true },
+  {
+    $set: {
+      keywords: ["goat", "laugh"],
+      startTime: new Date("2020-07-03T12:00:00Z"),
+      endTime: new Date("2020-07-03T12:03:00Z"),
+      resultSentence:
+        "He gives his goat a shake, And laughs until her belly aches.",
+      isCompleted: false,
+      status: "Sentence generation complete",
+    },
+  },
+  {
+    upsert: true,
+  }
+);
