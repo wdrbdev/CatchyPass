063f71d2d129024a743d108c388d41c402abe605
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-02 21:28:06 +0800

    feat(server): Initialize for MongoDB & Redis setup

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+// Import configurations
 const config = require("./config");
 
 const app = express();
 
 /*
- * Middleware
+ * Middlewares
  */
 app.use(cors()); // Connect to different ports such as React
 app.use(bodyParser.json());
@@ -16,6 +17,41 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Hello there!");
 });
+
+/*
+ * Initialize Redis
+ */
+const redis = require("redis");
+const client = redis.createClient({
+  host: config.redisHost,
+  port: config.redisPort,
+  retry_strategy: () => 1000,
+});
+// SUB/PUB mode for workers receiving regular commands
+const subscriber = redis.duplicate();
+const publisher = redis.duplicate();
+const redisObj = {
+  client,
+  subscriber,
+  publisher,
+};
+
+/*
+ * Initialize MongoDB
+ */
+mongoose.connect(
+  `mongodb://${config.mongoUser}:${config.mongoPassword}@${config.mongoHost}:${config.mongoPORT}`
+);
+
+// Load schema
+require("./models/Password.js");
+require("./models/Sentence.js");
+
+/*
+ * Register route handlers
+ */
+require("./routes/dbRoutes")(app);
+require("./routes/mqRoutes")(app, redisObj);
 
 const PORT = process.env.PORT || 5000;
 
