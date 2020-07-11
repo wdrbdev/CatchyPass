c1005a441d05435f1eeec0f62a3b2b07a271bfb8
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-11 20:43:52 +0800

    feat(ci): Build mongo image from docker hub

--- repo/img2file.sh
+++ repo/img2file.sh
@@ -1,12 +1,12 @@
 # Pull images from docker hub
-docker pull mongo:latest
+docker pull registry.hub.docker.com/library/mongo:latest
 docker pull tensorflow/tensorflow:latest-jupyter
 docker pull node:alpine
 docker pull redis:latest
 docker pull nginx:latest
 
 # Save images as file
-docker save -o ./repo/mongo.tar mongo:latest
+docker save -o ./repo/mongo.tar registry.hub.docker.com/library/mongo:latest
 docker save -o ./repo/tensorflow.tar tensorflow/tensorflow:latest-jupyter
 docker save -o ./repo/node.tar node:alpine
 docker save -o ./repo/redis.tar redis:latest
