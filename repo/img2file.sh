101b8ffa9c84bfbd9e061dd76ce5148c9b49fd27
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-11 11:54:25 +0800

    feat(docker): Add scripts for loading local image files

---
+++ repo/img2file.sh
@@ -0,0 +1,13 @@
+# Pull images from docker hub
+docker pull mongo:latest
+docker pull tensorflow/tensorflow:latest-jupyter
+docker pull node:alpine
+docker pull redis:latest
+docker pull nginx:latest
+
+# Save images as file
+docker save -o ./repo/mongo.tar mongo:latest
+docker save -o ./repo/tensorflow.tar tensorflow/tensorflow:latest-jupyter
+docker save -o ./repo/node.tar node:alpine
+docker save -o ./repo/redis.tar redis:latest
+docker save -o ./repo/nginx.tar nginx:latest
