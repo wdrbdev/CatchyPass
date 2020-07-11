67447974affd84fa5945b866ef46ed691fdb4c36
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-11 11:54:53 +0800

    fix(server): Correct typos

--- server/config.js
+++ server/config.js
@@ -2,8 +2,8 @@ module.exports = {
   mongoHost: process.env.MONGO_HOST,
   mongoUser: process.env.MONGO_USER,
   mongoPassword: process.env.MONGO_PASSWORD,
-  mongoDATABASE: process.env.MONGO_DATABASE,
-  mongoPORT: process.env.MONGO_PORT,
+  mongoDatabase: process.env.MONGO_DATABASE,
+  mongoPort: process.env.MONGO_PORT,
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
 };
