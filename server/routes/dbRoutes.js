faabcdea20533b7d936be4f0189b513ca89e7d8e
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-15 17:14:48 +0800

    feat(server): Replace all models by result model

--- server/routes/dbRoutes.js
+++ server/routes/dbRoutes.js
@@ -1,3 +1,10 @@
-module.exports = (app)=>{
-  
-}
+const mongoose = require("mongoose");
+const Result = mongoose.model("Result");
+const config = require("../config");
+
+module.exports = (app) => {
+  app.post("/result", async (req, res) => {
+    const result = await Result.findById(req.body._id);
+    res.json(result);
+  });
+};
