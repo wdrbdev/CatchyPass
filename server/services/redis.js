de21dc078aadfb2ae51c8d4976722c3512cec714
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-15 17:18:56 +0800

    feat(server): Separate codes for each channel

--- server/services/redis.js
+++ server/services/redis.js
@@ -2,8 +2,7 @@ const redis = require("redis");
 const mongoose = require("mongoose");
 const config = require("../config");
 const sent2pass = require("./sent2pass");
-const Sentence = mongoose.model("Sentence");
-const Password = mongoose.model("Password");
+const Result = mongoose.model("Result");
 
 module.exports = () => {
   const client = redis.createClient({
@@ -18,70 +17,32 @@ module.exports = () => {
   const publisher = client.duplicate();
 
   subscriber.subscribe("sentence");
+  subscriber.subscribe("password");
   subscriber.on("message", async (channel, message) => {
-    switch (channel) {
-      case "sentence":
-        console.log(message);
-        const { _id, status, resultSentence } = JSON.parse(message);
-        const sentence = await Sentence.findByIdAndUpdate(
-          { _id },
-          {
-            $set: {
-              resultSentence,
-              status,
-              endTime: Date.now(),
-              isCompleted: true,
-            },
-          }
-        );
-        console.log(sentence);
-        publisher.publish("password", JSON.stringify(sentence));
-        break;
-      case "password":
-        const { sentenceResult } = JSON.parse(message);
-        const resultPassword = sent2pass(sentenceResult);
-
-        const password = new Password({
-          $set: {
-            resultPassword,
-            isCompleted: true,
-            status: "testing",
-          },
-        });
-        try {
-          // Save the current data
-          await password.save();
-
-          await Sentence.findByIdAndUpdate(
-            { _id: sentenceResult._id },
-            {
-              $set: {
-                _password: password._id,
-              },
-            }
-          );
-
-          res.send(200);
-        } catch (err) {
-          res.send(400, err);
-        }
-        break;
+    if (channel === "sentence") {
+      let { _id, status, sentenceResult } = JSON.parse(message);
+      let sentence = await Result.findByIdAndUpdate(_id, {
+        $set: {
+          sentenceResult,
+          status,
+        },
+      });
+      publisher.publish("password", JSON.stringify(sentence));
     }
   });
 
-  // Test only (for publishing message)
-  // setTimeout(() => {
-  //   const kwList = ["kw1", "kw2", "k2"];
-  //   for (let i = 1; i <= 10; i++) {
-  //     setTimeout(() => {
-  //       publisher.publish(
-  //         "keywords",
-  //         JSON.stringify({ keywords: kwList, timeStamp: i * 2 })
-  //       );
-  //       console.log("keywords published");
-  //     }, 8000);
-  //   }
-  // }, 1000);
+  subscriber.on("message", async (channel, message) => {
+    if (channel === "password") {
+      let { _id, sentenceResult } = JSON.parse(message);
+      let passwordResult = sent2pass(sentenceResult);
+      await Result.findByIdAndUpdate(_id, {
+        $set: {
+          passwordResult,
+          endTime: Date.now(),
+        },
+      });
+    }
+  });
 
   // Return all redis objects
   const redisObj = {
