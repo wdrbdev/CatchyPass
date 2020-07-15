e65ffaf05ac00c22e26d787180d756356bf595a3
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-15 17:15:57 +0800

    feat(client): Add toutorial page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import CatchyPass from "./components/CatchyPass";
 import Header from "./components/Header";
 import About from "./components/About";
+import Tutorial from "./components/Tutorial";
 import "./sass/bulmastyles.scss";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Route exact={true} path="/" component={CatchyPass} />
         <Route exact={true} path="/about" component={About} />
+        <Route exact={true} path="/tutorial" component={Tutorial} />
       </BrowserRouter>
     </div>
   );
