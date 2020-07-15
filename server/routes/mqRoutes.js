89e759170453579fd8e8b81c0887ac3d84a9f8e4
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-07-15 17:14:48 +0800

    feat(server): Replace all models by result model

--- server/routes/mqRoutes.js
+++ server/routes/mqRoutes.js
@@ -1,39 +1,28 @@
 const mongoose = require("mongoose");
-const Sentence = mongoose.model("Sentence");
-const Password = mongoose.model("Password");
+const Result = mongoose.model("Result");
 const config = require("../config");
 
 module.exports = (app, { client, subscriber, publisher }) => {
   app.post("/keywords", async (req, res) => {
-    console.log(req.body);
     const { keywords } = req.body;
-    const sentence = new Sentence({
+    const result = new Result({
       keywords,
     });
-
     try {
       // Save the current data
-      await sentence.save();
+      await result.save();
 
       // Publish keywords input to message queue
       publisher.publish(
         "keywords",
         JSON.stringify({
-          _id: sentence._id,
+          _id: result._id,
           keywords,
         })
       );
-
-      res.sendStatus(200);
+      res.json(result);
     } catch (err) {
       res.sendStatus(400, err);
     }
-  });
-
-  app.post("/sentence", (req, res) => {
-    // TODO get sentence from db
-    // TODO convert sentence to password
-    // TODO save password to db
-    // TODO respond password generated
   });
 };
