b0f44f560ac54481fecf0edad2204f59184f5715
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-08-15 21:51:05 +0800

    feat(server): Initialize implementation of sent2psss

--- server/services/sent2pass.js
+++ server/services/sent2pass.js
@@ -1,3 +1,52 @@
-module.exports = (sentence) => {
-  return `password for ${sentence}`;
+module.exports = (
+  sentences,
+  keyword,
+  delimiter = "",
+  toLeet = true,
+  wordIndex = -1,
+  lineIndexes = [0, 1, 4]
+) => {
+  const REGEX = /[-!$%^&*()_+|~=`{}[:;<>?,.@#\]]/g;
+  const LEET_DICT = {
+    a: 4,
+    b: 8,
+    e: 3,
+    g: 6,
+    l: 1,
+    s: 5,
+    t: 7,
+    z: 2,
+  };
+
+  const words = sentences
+    .split("\n")
+    .map((line, index) => {
+      if (lineIndexes.includes(index)) return line.replace(REGEX, "");
+    })
+    .filter((elem) => {
+      return typeof elem === "string";
+    })
+    .map((line) => {
+      return line
+        .split(" ")
+        .filter((word) => word !== "")
+        .slice(wordIndex);
+    });
+
+  if (toLeet) {
+    keyword = keyword
+      .split("")
+      .map((c) => {
+        if (c in LEET_DICT) return LEET_DICT[c];
+        return c;
+      })
+      .join("");
+  }
+  
+  return [keyword, ...words]
+    .flat(1)
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(delimiter);
 };
