77e210babdad69918e9a1e1d3e92ed4f1d914d47
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-08-19 00:23:56 +0800

    feat(client): Initialize about and tutorial

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,16 @@ import React from "react";
 const About = () => {
   return (
     <div className="container">
-      <div className="title">About</div>
+      <div className="mt-2">
+        <div className="title is-3">CatchyPass - About</div>
+        <div className="is-size-5">
+          A web application for strong and catchy password generation from
+          keyword input with natural language processing. The goal of the app is
+          to generate a strong password with an easy-to-remember limerick
+          according to keywords provided by the user to enhance password
+          usability while preserving password strength.
+        </div>
+      </div>
     </div>
   );
 };
