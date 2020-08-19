172ebd877fef979012cce24cf1248104b7c2188a
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-08-20 00:43:50 +0800

    test(ci): Test integration testing on Cloud Build

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,15 +1,29 @@
 const puppeteer = require("puppeteer");
 const axios = require("axios");
 const config = require("./config");
-const testURL = `http://${config.minikubeIP}`;
+const rootUrl = `http://${config.reservedIp}`;
 
-test("Init test", () => {
-  console.log(`minikube ip = ${config.minikubeIP}`);
-  expect(1).toEqual(1);
+beforeEach(async () => {
+  jest.setTimeout(100000);
+  await page.goto(rootUrl);
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+test("Test IP address in env variable.", () => {
+  expect(config.reservedIp).toEqual("35.220.154.72");
 });
 
 test("Test whether API root URL is available.", async () => {
-  console.log(`Testing ${testURL}/api`);
-  const res = await axios.get(`${testURL}/api`);
+  console.log(`Testing ${rootUrl}/api`);
+  const res = await axios.get(`${rootUrl}/api`);
   expect(res.data).toEqual("Hello there!");
 });
+
+// describe("When user go to the website,", () => {
+//   beforeEach(async () => {
+//   });
+
+// });
