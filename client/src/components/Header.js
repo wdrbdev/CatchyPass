f0dad3aea12ad22f4d364de1c248bf7faf53aeb9
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-08-19 15:16:45 +0800

    feat(client): Add event for burger & navbar-menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../logo.png";
 
 const Header = () => {
+  /*
+   * Activate navbar-burger
+   * https://bulma.io/documentation/components/navbar/
+   */
+  useEffect(() => {
+    document.addEventListener("DOMContentLoaded", () => {
+      // Get all "navbar-burger" elements
+      const $navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll(".navbar-burger"),
+        0
+      );
+
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach((el) => {
+          el.addEventListener("click", () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle("is-active");
+            $target.classList.toggle("is-active");
+
+            document
+              .querySelector(".navbar-menu")
+              .classList.toggle("is-active");
+          });
+        });
+      }
+    });
+  });
+
   return (
     <nav
       className="navbar is-primary container"
