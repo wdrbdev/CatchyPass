a2a6418a192b202367d84e055fc85c94fbf07ca7
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-08-19 15:35:14 +0800

    feat(client): Add customized onClick

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,13 +19,9 @@ const Header = () => {
         // Add a click event on each of them
         $navbarBurgers.forEach((el) => {
           el.addEventListener("click", () => {
-            // Get the target from the "data-target" attribute
-            const target = el.dataset.target;
-            const $target = document.getElementById(target);
-
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            el.classList.toggle("is-active");
-            $target.classList.toggle("is-active");
+            document
+              .querySelector(".navbar-burger")
+              .classList.toggle("is-active");
 
             document
               .querySelector(".navbar-menu")
