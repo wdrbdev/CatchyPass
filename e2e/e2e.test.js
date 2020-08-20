009698ed4dea777085bc1769e111b4612aa7fa26
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-08-20 10:45:18 +0800

    test(ci): Test integration testing on Cloud Build

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -3,8 +3,15 @@ const axios = require("axios");
 const config = require("./config");
 const rootUrl = `http://${config.reservedIp}`;
 
+const browser = await puppeteer.launch({
+  headless: true,
+});
+let page;
+
 beforeEach(async () => {
   jest.setTimeout(100000);
+
+  page = await browser.newPage();
   await page.goto(rootUrl);
 });
 
@@ -22,8 +29,13 @@ test("Test whether API root URL is available.", async () => {
   expect(res.data).toEqual("Hello there!");
 });
 
-// describe("When user go to the website,", () => {
-//   beforeEach(async () => {
-//   });
+describe("When users go to the website,", () => {
+  test("Users will see project name",()=>{
+    const projectName = await page.$eval(
+      ".navbar-item .is-size-4",
+      (elem) => elem.innerHTML
+    );
+    expect(projectName).toEqual("CatchyPass")
+  })
 
-// });
+});
