085a6645a282ba4d52475a6a64be10256f695d10
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2020-09-22 08:37:19 +0800

    feat(worker): Update unit tests for list

--- worker/keyword2text.py
+++ worker/keyword2text.py
@@ -8,10 +8,13 @@ def keyword2text(keywords):
     model_path = './data/catchypass/trained_model/pytorch_model.bin'
     ai = aitextgen(model=model_path, config=config_path)
 
-    keyword = keywords.lower()  # keyword = " ".join(keywords).lower()
+    keyword = " ".join(keywords).lower()
+    for k in keywords:
+        k = f'#{k}'
+    prefix = " ".join(keywords).lower() + ": "
+
     N_RETRY = 0
     retry_count = 0
-    prefix = f'#{keyword}:'
     while retry_count <= N_RETRY:
         texts = ai.generate(n=1,
                             prompt=prefix,
@@ -21,9 +24,9 @@ def keyword2text(keywords):
         for text in texts:
             text = text.replace(" <|n|> ", "\n")
             if text.lower().find(keyword) != -1:
-                return text.replace(prefix + " ", "")
+                return text.replace(prefix, "")
         retry_count = retry_count + 1
 
     text = ai.generate(n=1, prompt=prefix, max_length=256,
                        return_as_list=True)[0]
-    return text.replace(" <|n|> ", "\n").replace(prefix + " ", "")
+    return text.replace(" <|n|> ", "\n").replace(prefix, "")
--- worker/unit_testing.py
+++ worker/unit_testing.py
@@ -5,7 +5,7 @@ from keyword2text import keyword2text
 class TestLimerickGeneration(unittest.TestCase):
     # Limerick output should contains 5 lines according to limerick format
     def test_limerick_generation(self):
-        limerick = keyword2text("flower")
+        limerick = keyword2text(["flower"])
         print(limerick)
         limerick_length = len(limerick.split("\n"))
         self.assertEqual(5, limerick_length)
